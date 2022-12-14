6df62a8c02b27cf2aedef0e0b007224dc5d366ed
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-14 09:16:16 +0300

    added strict and slightly fixed stuff

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.route.js";
 import blogRouter from "./routes/blog.route.js";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
+import { MongooseError } from "mongoose";
 
 dotenv.config({
   path: ".env",
@@ -27,18 +28,24 @@ app.use("/blog", blogRouter);
 app.use("/post", postRouter);
 
 const port = process.env.APP_PORT || "5000";
+const url = process.env.DB_URL;
 
 const start = async () => {
   try {
-    await connectToDB(process.env.DB_URL);
+    if (!url) throw new Error("DB_URL environment variable is not set.");
+    await connectToDB(url);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
   } catch (error) {
-    if (error.name === "MongooseServerSelectionError") {
-      console.error(`Can't connect to db ${process.env.DB_URL}`);
+    if (error instanceof MongooseError) {
+      if (error.name === "MongooseServerSelectionError") {
+        console.error(`Can't connect to db ${process.env.DB_URL}`);
+      } else {
+        console.error("Mongoose error: ", error);
+      }
     } else {
-      console.error(`${error}`);
+      console.error("Server error: ", error);
     }
   }
 };
