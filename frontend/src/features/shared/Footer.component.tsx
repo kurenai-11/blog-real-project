d4d0fcf357fc6543c73346779e69753a2709c839
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-15 06:58:58 +0300

    design improvements

--- frontend/src/features/shared/Footer.component.tsx
+++ frontend/src/features/shared/Footer.component.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="my-4 text-lg flex justify-center gap-1">
+    <div className="py-3 text-lg flex justify-center gap-1 bg-zinc-9">
       Made by
       <a
         href="https://github.com/kurenai-11"
