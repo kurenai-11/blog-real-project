594fcf96e1d8121ca70573d26cd2772b69c8be73
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-17 13:59:53 +0300

    experimentally generalize tailwind component creation

--- frontend/src/features/auth-page/FormButton.component.tsx
+++ frontend/src/features/auth-page/FormButton.component.tsx
@@ -1,20 +1,21 @@
 import {
-  createTailwindButtonComponent,
-  ImplementedButtonProps,
+  createTailwindComponent,
+  ImplementedElementProps,
 } from "../shared/utils";
 
 const FormButton = ({
-  children,
   additionalClasses,
+  children,
   ...reactProps
-}: ImplementedButtonProps) => {
+}: ImplementedElementProps<"button">) => {
   const baseClasses =
     "flex justify-center items-center gap-1 outline-none border-none py-2 px-4 my-2 rounded-2 bg-coolGray-6 text-zinc-2 text-lg cursor-pointer transition-all hover:( bg-coolgray-7 text-zinc-1 )";
-  return createTailwindButtonComponent({
-    reactProps,
+  return createTailwindComponent({
+    elementType: "button",
     baseClasses,
     additionalClasses,
     children,
+    reactProps,
   });
 };
 
--- frontend/src/features/shared/utils.tsx
+++ frontend/src/features/shared/utils.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { twMerge } from "tailwind-merge";
 
 //
@@ -96,3 +97,41 @@ export const createTailwindAComponent = ({
     </a>
   );
 };
+
+// experimental
+
+type CreateProps<T extends keyof JSX.IntrinsicElements> = {
+  elementType: T;
+  reactProps: JSX.IntrinsicElements[T];
+  baseClasses: string;
+  additionalClasses?: string;
+  children?: React.ReactNode;
+};
+
+export const createTailwindComponent = <T extends keyof JSX.IntrinsicElements>({
+  elementType,
+  reactProps,
+  baseClasses,
+  additionalClasses,
+  children,
+}: CreateProps<T>) => {
+  const Element = React.createElement(
+    elementType,
+    {
+      ...reactProps,
+      className: twMerge(baseClasses, additionalClasses),
+    },
+    children
+  );
+  return Element;
+};
+
+export type ImplementedElementProps<T extends keyof JSX.IntrinsicElements> =
+  JSX.IntrinsicElements[T] & {
+    children: React.ReactNode;
+    additionalClasses?: string;
+  };
+// invocation
+
+// export const ExperimentalComponent = (props) =>
+// createTailwindComponent("a", props, "", "");
