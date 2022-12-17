0d997cf48bd93e8e34976a001d6d2057b4daad68
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-17 12:05:54 +0300

    delete redundancy

--- frontend/src/features/dashboard-page/AddBlogModal.component.tsx
+++ frontend/src/features/dashboard-page/AddBlogModal.component.tsx
@@ -41,18 +41,11 @@ const AddBlogModal = () => {
   return (
     <div
       id="addBlog"
-      className={twMerge(
-        // z--1 - below everything z-0 main content of the page
-        // z-1 above main content of the page
-        "fixed z--1 top-0 left-0 w-full h-full bg-zinc-9 bg-opacity-0 opacity-0 transition-all duration-300",
-        "target:(opacity-100 bg-opacity-60 block z-1)"
-      )}
+      // z--1 - below everything z-0 main content of the page
+      // z-1 above main content of the page
+      className="fixed z--1 top-0 left-0 w-full h-full bg-zinc-9 bg-opacity-0 opacity-0 transition-all duration-300 target:(opacity-100 bg-opacity-60 block z-1)"
     >
-      <div
-        className={twMerge(
-          "w-[80vw] bg-zinc-9 fixed z-2 left-10% top-20% items-center text-xl rounded-xl transition-all duration-1000 flex flex-col"
-        )}
-      >
+      <div className="w-[80vw] bg-zinc-9 fixed z-2 left-10% top-20% items-center text-xl rounded-xl transition-all duration-1000 flex flex-col">
         <a
           href="#"
           className="fixed top-20% right-10% w-12 h-12 text-4xl decoration-none flex justify-center items-center text-amber-3 outline-none border-none bg-red-5 rounded-xl cursor-pointer"
