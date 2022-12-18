7ea38414779b90a825d48ba9d2cb793906566b01
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-18 13:25:54 +0300

    quick fix

--- frontend/src/features/dashboard-page/Dashboard.component.tsx
+++ frontend/src/features/dashboard-page/Dashboard.component.tsx
@@ -40,7 +40,10 @@ const Dashboard = () => {
         <div className="text-4xl pb-4 font-bold">Dashboard</div>
         <div className="flex justify-center items-center rounded-full border border-zinc-2 w-28 h-28 bg-zinc-6 mb-2">
           {avatarUrl ? (
-            <img src={avatarUrl} className="w-full h-full rounded-full" />
+            <img
+              src={avatarUrl}
+              className="w-full h-full rounded-full object-cover"
+            />
           ) : (
             <span className="font-bold text-lg cursor-default select-none">
               No avatar
