33d2ec6fc88d3f03885d1b77a9995c65b64c1326
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2022-12-19 13:18:00 +0300

    started blog page

--- frontend/src/features/blog-page/BlogPage.component.tsx
+++ frontend/src/features/blog-page/BlogPage.component.tsx
@@ -2,8 +2,21 @@ import { Blog } from "../../app/types";
 
 const BlogPage = ({ blog }: { blog: Blog }) => {
   return (
-    <div>
-      <div>{blog.title}</div>
+    <div className="flex flex-col items-center mt-2 mb-4">
+      <div className="text-3xl mb-3">
+        <span className="text-blue-6 font-bold">{blog.title}</span> by{" "}
+        <span className="text-red-6 font-mono">{blog.authorName}</span>
+      </div>
+      {blog.posts.length === 0 ? (
+        <div className="text-center text-2xl">
+          There are no posts here yet...
+        </div>
+      ) : (
+        <div>
+          <div className="text-center text-2xl">Posts:</div>
+          {/* to render posts here... */}
+        </div>
+      )}
     </div>
   );
 };
