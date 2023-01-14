7b7161cc9c8f4ea6b4cf4f802fc7c4b2324f9323
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-14 15:50:49 +0300

    added vitest

---
+++ backend/src/app.test.ts
@@ -0,0 +1,27 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import dotenv from "dotenv";
+import app from "./app.js";
+import { connect as connectToDB } from "./db/db.js";
+import mongoose from "mongoose";
+
+describe("app", () => {
+  beforeAll(() => {
+    dotenv.config({ path: "backend/.env" });
+  });
+  afterAll(() => {
+    mongoose.disconnect();
+  });
+  it("should have MONGO_URL environment variable", async () => {
+    expect(process.env.MONGO_URL).toBeDefined();
+  });
+  it("should properly start http server", async () => {
+    const response = await request(app).get("/");
+    return expect(response.body).toEqual({
+      message: "yes the server is working",
+    });
+  });
+  it("should properly connect to the database.", async () => {
+    await expect(connectToDB(process.env.MONGO_URL!)).resolves.toBeDefined();
+  });
+});
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connect as connectToDB } from "./db/db.js";
 import userRouter from "./routes/user.route.js";
 import blogRouter from "./routes/blog.route.js";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
-import { Error } from "mongoose";
-import { initializeCounters } from "./db/counter.js";
 import { User } from "./models/user.model.js";
 import { Blog } from "./models/blog.model.js";
 import { Post } from "./models/post.model.js";
@@ -29,31 +26,11 @@ Comment;
 
 // Routes
 app.get("/", (_, res) => {
-  res.send("yes the server is working");
+  res.json({ message: "yes the server is working" });
 });
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/blog", blogRouter);
 app.use("/post", postRouter);
 
-const port = process.env.PORT || 5000;
-const url = process.env.MONGO_URL;
-
-// using iife to be fancy and start the server
-(async () => {
-  try {
-    if (!url) throw new Error("MONGO_URL environment variable is not set.");
-    if (!port) throw new Error("PORT is not set");
-    await connectToDB(url);
-    initializeCounters();
-    app.listen(port, () => {
-      console.log(`listening on port ${port}`);
-    });
-  } catch (error) {
-    if (error instanceof Error.MongooseServerSelectionError) {
-      console.error(`Can't connect to db ${process.env.DB_URL}`);
-    } else {
-      console.error("Server error: ", error);
-    }
-  }
-})();
+export default app;
