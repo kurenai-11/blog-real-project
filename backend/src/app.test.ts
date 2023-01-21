662ff2daa9f8b84e0de6016fa51a41db049572dc
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-21 18:38:16 +0300

    added more tests

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -24,8 +24,63 @@ describe("app", () => {
   it("should properly connect to the database.", async () => {
     await expect(connectToDB(process.env.MONGO_URL!)).resolves.toBeDefined();
   });
+  const FAKE_USER_ID = 0;
+  const FAKE_USER_AUTHKEY = "eb1f4c78-359c-4eb9-a3ed-65b4cfbe6ac6";
   describe("routes", () => {
-    describe.todo("blog route");
+    describe("blog route", () => {
+      describe("GET /blog/", () => {
+        let response: request.Response;
+        beforeAll(async () => {
+          response = await request(app).get("/blog");
+        });
+        it("should get recently created blogs without query parameters", () => {
+          expect(response.body.status).toBe("success");
+        });
+        it("should have blogs populated", () => {
+          expect(response.body.blogs.length).toBeGreaterThan(0);
+        });
+        it.todo("should work with query parameters");
+      });
+      describe("POST /blog", () => {
+        let createdBlogId: number;
+        it("should return 400 with the invalid request", async () => {
+          const response = await request(app).post("/blog").send({
+            title: 15,
+            description: "hey",
+            userId: 12,
+            authKey: "hey hey",
+          });
+          expect(response.statusCode).toEqual(400);
+        });
+        it("should check auth key to create a new blog", async () => {
+          const response = await request(app).post("/blog").send({
+            title: "a test blog",
+            description: "a test blog description",
+            userId: 0,
+            authKey: "hey hey",
+          });
+          expect(response.statusCode).toEqual(401);
+        });
+        it("should be able to create a new blog", async () => {
+          const response = await request(app).post("/blog").send({
+            title: "a test blog",
+            description: "a test blog description",
+            userId: FAKE_USER_ID,
+            authKey: FAKE_USER_AUTHKEY,
+          });
+          createdBlogId = response.body.blogId;
+          expect(response.statusCode).toEqual(200);
+        });
+        afterAll(async () => {
+          const response = await request(app).delete("/blog/").send({
+            authKey: "eb1f4c78-359c-4eb9-a3ed-65b4cfbe6ac6",
+            userId: FAKE_USER_ID,
+            blogId: createdBlogId,
+          });
+          expect(response.statusCode).toBe(200);
+        });
+      });
+    });
     describe.todo("post route");
     describe.todo("user route");
   });
