dc1de3df69f5fe2edaf13ec3b3378caadf25afb6
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-27 10:47:32 +0300

    made further customizable

--- frontend/src/features/shared/Loading.component.tsx
+++ frontend/src/features/shared/Loading.component.tsx
@@ -1,9 +1,15 @@
 import { Triangle } from "react-loader-spinner";
 
-const Loading = ({ size = 150 }: { size?: number }) => {
+const Loading = ({
+  size = 150,
+  color = "#b11628",
+}: {
+  size?: number;
+  color?: string;
+}) => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <Triangle height={size} width={size} color="#b11628" />
+      <Triangle height={size} width={size} color={color} />
     </div>
   );
 };
