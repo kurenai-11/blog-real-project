b3adceaa7e2404b3fe125ba6faf701dc92275a69
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-27 10:44:05 +0300

    customizable size

--- frontend/src/features/shared/Loading.component.tsx
+++ frontend/src/features/shared/Loading.component.tsx
@@ -1,9 +1,9 @@
 import { Triangle } from "react-loader-spinner";
 
-const Loading = () => {
+const Loading = ({ size = 150 }: { size?: number }) => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <Triangle height={150} width={150} color="#b11628" />
+      <Triangle height={size} width={size} color="#b11628" />
     </div>
   );
 };
